38d1d2876a3b68c14f8e2750c89f2a0e4a8a148c
Sushruth Sastry <susastry@microsoft.com>
2020-07-18 04:00:44 -0700

    different approach + updated readme

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ deno run --allow-read --allow-write https://raw.githubusercontent.com/sushruth/y
 ```
 
 and now check for `yarn.duplicates.log.json`
+
+> WARNING: this tool only tries to pin the resolution on the latest version required for any package. Does not guarantee operation without issues.
--- index.ts
+++ index.ts
@@ -1,36 +1,47 @@
-const lockContents = Deno.readTextFileSync("yarn.lock");
+import * as semver from "https://deno.land/x/semver/mod.ts";
 
+type ParsedList = null | undefined | Record<string, string[]>;
+type SuggestionLst = Record<string, string>;
 
-if (lockContents === null) {
+let lockContents: string | undefined;
+try {
+  lockContents = Deno.readTextFileSync("yarn.lock");
+} catch (error) {}
+
+if (!lockContents) {
   throw Error("File contents not there");
 }
 
+const mainRegex = /^\S.*:\s*\n\s*version "(.*?)"\s*$/mg;
+
 const parsedList = lockContents
-  .match(/^\S(.*?):$/gm)
-  ?.map((value) => value.replace(/("|:|\s)/gm, ""))
-  .reduce((acc: Record<string, Set<string>>, entry) => {
-    const values = entry.split(",");
-    for (const value of values) {
-      const versionMatches = value.match(/([^\s]*)@(.*?)$/);
-      if (versionMatches) {
-        const [, packageName, version] = versionMatches;
-        if (acc[packageName]) {
-          acc[packageName].add(version);
-        } else {
-          acc[packageName] = new Set([version]);
-        }
+  .match(mainRegex)
+  ?.map(v => v.replace(/\s*|\n*/g, "").replace(/"/g, ""))
+  .reduce((acc: Record<string, string[]>, entry) => {
+    const packageName = entry.split(',')[0].replace(/(?!^)@(.*?)version(.*?)$/g, '');
+    const version = entry.match(/:version(.*?)$/)?.[1];
+    console.debug(entry.split(',')[0]);
+    if (version) {
+      if (acc[packageName] && !acc[packageName].includes(version)) {
+        acc[packageName].push(version);
+      } else {
+        acc[packageName] = [version];
       }
     }
     return acc;
   }, {});
 
-const packageVersions: Record<string, string[]> = {};
+const multiples: ParsedList = {};
+const resolutions: SuggestionLst = {};
 
-for (const item in parsedList) {
-  packageVersions[item] = Array.from(parsedList[item]);
+for (const entry in parsedList) {
+  if (parsedList[entry].length > 1) {
+    multiples[entry] = parsedList[entry];
+    resolutions[entry] = parsedList[entry].sort(semver.rcompare)[0]
+  }
 }
 
 Deno.writeTextFileSync(
   "yarn.duplicates.log.json",
-  JSON.stringify(packageVersions, null, "  ")
+  JSON.stringify({ multiples, resolutions }, null, "  ")
 );
